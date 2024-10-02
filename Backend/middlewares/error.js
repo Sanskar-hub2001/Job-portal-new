e242d55dd5177fefcc030f607e98e0305e805cc6
Sanskar-hub2001 <sanskar20072001@gmail.com>
2024-10-02 17:02:35 +0530

    fixed error

--- Backend/middlewares/error.js
+++ Backend/middlewares/error.js
@@ -1,35 +1,74 @@
 
+// class ErrorHandler extends Error {
+//     constructor(message, statusCode) {
+//       super(message);
+//       this.statusCode = statusCode;
+//     }
+//   }
+  
+//   export const errorMiddleware = (err, req, res, next) => {
+//     err.message = err.message || "Internal Server Error";
+//     err.statusCode = err.statusCode || 500;
+  
+//     if (err.name === "CastError") {
+//       const message = `Resource not found. Invalid ${err.path}`,
+//         err = new ErrorHandler(message, 400);
+//     }
+//     if (err.code === 11000) {
+//       const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
+//         err = new ErrorHandler(message, 400);
+//     }
+//     if (err.name === "JsonWebTokenError") {
+//       const message = `Json Web Token is invalid, Try again!`;
+//       err = new ErrorHandler(message, 400);
+//     }
+//     if (err.name === "TokenExpiredError") {
+//       const message = `Json Web Token is expired, Try again!`;
+//       err = new ErrorHandler(message, 400);
+//     }
+//     return res.status(err.statusCode).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   };
+  
+//   export default ErrorHandler;
 class ErrorHandler extends Error {
-    constructor(message, statusCode) {
-      super(message);
-      this.statusCode = statusCode;
-    }
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
   }
-  
-  export const errorMiddleware = (err, req, res, next) => {
-    err.message = err.message || "Internal Server Error";
-    err.statusCode = err.statusCode || 500;
-  
-    if (err.name === "CastError") {
-      const message = `Resource not found. Invalid ${err.path}`,
-        err = new ErrorHandler(message, 400);
-    }
-    if (err.code === 11000) {
-      const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
-        err = new ErrorHandler(message, 400);
-    }
-    if (err.name === "JsonWebTokenError") {
-      const message = `Json Web Token is invalid, Try again!`;
-      err = new ErrorHandler(message, 400);
-    }
-    if (err.name === "TokenExpiredError") {
-      const message = `Json Web Token is expired, Try again!`;
-      err = new ErrorHandler(message, 400);
-    }
-    return res.status(err.statusCode).json({
-      success: false,
-      message: err.message,
-    });
-  };
-  
-  export default ErrorHandler;
+}
+
+export const errorMiddleware = (err, req, res, next) => {
+  err.message = err.message || "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
+
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);  // This creates a new ErrorHandler instance, so you need to pass it directly.
+  }
+
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);  // Similarly, create a new instance here
+  }
+
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, Try again!`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token is expired, Try again!`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Final response
+  return res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
+
+export default ErrorHandler;
